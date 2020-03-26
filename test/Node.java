3b4a7fa7ebf0862c21b49a11861275b0b0d1552f
unknown <yu17782837186>
2020-03-26 23:03:19 +0800

    test

---
+++ test/Node.java
@@ -0,0 +1,13 @@
+package com.ning.linked;
+
+public class Node {
+    Object data;
+    Node next;
+
+    public Node(Object data) {
+        this.data = data;
+    }
+
+    public Node() {
+    }
+}
